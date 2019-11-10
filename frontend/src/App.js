8cc4cb177a5dc54f7e50e9df38158c6cc1d9e845
yehmond <yehmondray@gmail.com>
2019-11-10 00:47:18 -0800

    setup frontend skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import Vehicles from "./pages/Vehicles";
+import Navbar from "./Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <Navbar/>
+            <Switch>
+                <Route exact path={"/vehicles"} component={Vehicles}/>
+                <Route exact path={"/"} component={Vehicles}/>
+            </Switch>
+        </React.Fragment>
+    );
 }
 
 export default App;
---
+++ frontend/src/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">SuperRent</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/vehicles">Vehicles<span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Reserve</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Daily Reports</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Tables
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="dropdown-item" href="#">Reservations</a>
+                                <a className="dropdown-item" href="#">Rentals</a>
+                                <a className="dropdown-item" href="#">Vehicles</a>
+                                <a className="dropdown-item" href="#">VehicleTypes</a>
+                                <a className="dropdown-item" href="#">Customers</a>
+                                <a className="dropdown-item" href="#">Returns</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+
+}
+
+export default Navbar;
